a551dcbcb38e1498d5e430702cab2cd7d431c3a0
celesca <folk.sawit@gmail.com>
2025-02-11 22:00:22 +0700

    add: unit testing

--- product-service/src/test/java/com/celesca/microservices/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/celesca/microservices/product_service/ProductServiceApplicationTests.java
@@ -1,7 +1,12 @@
 package com.celesca.microservices.product_service;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MongoDBContainer;
 
@@ -9,9 +14,43 @@ import org.testcontainers.containers.MongoDBContainer;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
 	void shouldCreateProduct() {
+		String requestBody = """
+				{
+				    "name": "IPhone 15",
+				    "description": "iPhone 15 is the phone from apple",
+				    "price": 1000
+				}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("IPhone 15"))
+				.body("description", Matchers.equalTo("iPhone 15 is the phone from apple"))
+				.body("price", Matchers.equalTo(1000));
 	}
 
 }
