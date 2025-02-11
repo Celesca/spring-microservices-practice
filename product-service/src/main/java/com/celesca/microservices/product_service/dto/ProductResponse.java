5676fe5b4517a40a50a6ffbad924f2d1cafb2781
celesca <folk.sawit@gmail.com>
2025-02-11 20:09:39 +0700

    change to productresponse

---
+++ product-service/src/main/java/com/celesca/microservices/product_service/dto/ProductResponse.java
@@ -0,0 +1,6 @@
+package com.celesca.microservices.product_service.dto;
+
+import java.math.BigDecimal;
+
+public record ProductResponse(String id, String name, String description, BigDecimal price) {
+}
