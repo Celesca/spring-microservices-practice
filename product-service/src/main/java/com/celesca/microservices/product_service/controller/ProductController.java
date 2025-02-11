4bc366054bf4440c799be63284c11234d49c35ec
celesca <folk.sawit@gmail.com>
2025-02-11 20:00:02 +0700

    add: productController

--- product-service/src/main/java/com/celesca/microservices/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/celesca/microservices/product_service/controller/ProductController.java
@@ -1,16 +1,23 @@
 package com.celesca.microservices.product_service.controller;
 
 import com.celesca.microservices.product_service.dto.ProductRequest;
+import com.celesca.microservices.product_service.model.Product;
+import com.celesca.microservices.product_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductRequest productRequest) {
+    public Product createProduct(@RequestBody ProductRequest productRequest) {
         // Create product
+        return productService.createProduct(productRequest);
     }
 }
--- product-service/src/main/java/com/celesca/microservices/product_service/service/ProductService.java
+++ product-service/src/main/java/com/celesca/microservices/product_service/service/ProductService.java
@@ -4,19 +4,23 @@ import com.celesca.microservices.product_service.dto.ProductRequest;
 import com.celesca.microservices.product_service.model.Product;
 import com.celesca.microservices.product_service.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public void createProduct(ProductRequest productRequest) {
+    public Product createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.name())
                 .description(productRequest.description())
                 .price(productRequest.price())
                 .build();
         productRepository.save(product);
+        log.info("Product created successfully");
+        return product;
     }
 }
