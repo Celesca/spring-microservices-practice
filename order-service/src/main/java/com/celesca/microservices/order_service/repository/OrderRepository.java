2e8d10ea04ef1f5e98d75f510229b1b68b694d7d
celesca <folk.sawit@gmail.com>
2025-02-12 22:27:34 +0700

    add: Order Controller

--- order-service/src/main/java/com/celesca/microservices/order_service/repository/OrderRepository.java
+++ order-service/src/main/java/com/celesca/microservices/order_service/repository/OrderRepository.java
@@ -4,4 +4,5 @@ import com.celesca.microservices.order_service.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }
